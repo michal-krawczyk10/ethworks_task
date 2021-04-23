8e1fed7cc82db572d1640f0a7f69e6c7ddd94b66
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-04-23 04:38:28 +0200

    feat: add js for toggle modal button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import _ from "lodash";
 import "./style.scss";
 
+const playBtn = document.querySelector(".play--js");
+const modal = document.querySelector(".modal__vid");
+const body = document.querySelector(".body--js");
+
+playBtn.addEventListener("click", () => {
+	console.log("test");
+	modal.classList.add("show");
+	body.classList.add("noscroll");
+});
