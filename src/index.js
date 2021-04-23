81100bf557f6bf9631da42398f3a07a9e1bbe98c
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-04-23 09:54:30 +0200

    fix: layout issues, team ready

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ const modal = document.querySelector(".modal");
 const modalVid = document.querySelector(".modal__vid");
 const body = document.querySelector(".body--js");
 const video = document.querySelector(".video--js");
+
 const tM1 = document.querySelector(".tm1--js");
-// const tM2 = document.querySelector(".tm2--js");
-// const tM3 = document.querySelector(".tm3--js");
-// const tM4 = document.querySelector(".tm4--js");
+const tM2 = document.querySelector(".tm2--js");
+const tM3 = document.querySelector(".tm3--js");
+const tM4 = document.querySelector(".tm4--js");
+
 const modalTM1 = document.querySelector(".modal__tm1--js");
-// const modalTM2 = document.querySelector(".modal__tm2--js");
-// const modalTM3 = document.querySelector(".modal__tm4--js");
-// const modalTM4 = document.querySelector(".modal__tm4--js");
+const modalTM2 = document.querySelector(".modal__tm2--js");
+const modalTM3 = document.querySelector(".modal__tm3--js");
+const modalTM4 = document.querySelector(".modal__tm4--js");
+
+const closeBtn = document.querySelectorAll(".close--js");
 
 playBtn.addEventListener("click", () => {
 	modalVid.classList.add("show");
@@ -28,3 +32,24 @@ modal.addEventListener("click", () => {
 tM1.addEventListener("click", () => {
 	modalTM1.classList.add("show");
 });
+
+tM2.addEventListener("click", () => {
+	modalTM2.classList.add("show");
+});
+
+tM3.addEventListener("click", () => {
+	modalTM3.classList.add("show");
+});
+
+tM4.addEventListener("click", () => {
+	modalTM4.classList.add("show");
+});
+
+closeBtn.forEach((input) =>
+	input.addEventListener("click", () => {
+		modalTM1.classList.remove("show");
+		modalTM2.classList.remove("show");
+		modalTM3.classList.remove("show");
+		modalTM4.classList.remove("show");
+	})
+);
